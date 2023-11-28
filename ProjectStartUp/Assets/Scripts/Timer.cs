2f1dc5ad375265be03a84541625b7c525b4521c5
272Jayden <jaydenfokkink@gmail.com>
2023-11-28 14:34:05 +0100

    added timer, break timer, and sessions
    the user can incresease the timer the break timer and the amount of sessions

--- ProjectStartUp/Assets/Scripts/Timer.cs
+++ ProjectStartUp/Assets/Scripts/Timer.cs
@@ -2,45 +2,119 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Microsoft.Unity.VisualStudio.Editor;
+using UnityEngine.UIElements;
+using Unity.VisualScripting;
 
 public class Timer : MonoBehaviour
 {
+    //ui
     [SerializeField] private TMP_Text focusTimer;
+    [SerializeField] private TMP_Text breakTimer;
+    [SerializeField] private TMP_Text sessionCounter;
+    UnityEngine.UI.Image panel;
+    int alphaColor = 1;
 
-    public float minutes = 5;
+    //color break blue
+    Color breakBlue = new Color(0.4f, 0.8509f, 1);
+
+    //color focus red
+    Color focusRed = new Color(0.463f, 0.133f, 0.097f);
+
+    //color main green
+    Color mainGreen = new Color(0.392f, 0.643f, 0.349f);
+
+    //main timer
+    public float minutes;
+    public float rememberMinutes;
     public float seconds;
+    public float rememberSeconds;
     public int roundedSeconds;
 
+    //break timer
+    public float breakMinutes;
+    public float rememberBreakMinutes;
+    public float breakSeconds;
+    public float rememberBreakSeconds;
+    public int roundedBreakSeconds;
+
+    //session variables
+    public int sessionAmount;
+
+    //timer bools
     public bool timerHasStarted = false;
+    public bool breakTimerHasStarted = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        panel = GameObject.Find("BackgroundPanel").GetComponent<UnityEngine.UI.Image>();
     }
 
     // Update is called once per frame
     void Update()
     {
         roundedSeconds = Mathf.RoundToInt(seconds);
+        roundedBreakSeconds = Mathf.RoundToInt(breakSeconds);
         focusTimer.text = minutes + ":" + roundedSeconds;
+        breakTimer.text = breakMinutes + ":" + roundedBreakSeconds;
+        sessionCounter.text = "Sessions: " + sessionAmount;
 
-        if (timerHasStarted)
+        if(timerHasStarted )
         {
-            if (seconds < 1)
-            {
-                seconds = 59;
-                minutes--;
-            }
-            if (seconds > 0)
-            {
-                seconds = seconds - Time.deltaTime;
-            }
+            panel.color = focusRed;
+            breakMinutes = rememberBreakMinutes; 
+            breakSeconds = rememberBreakSeconds;
 
-            if(seconds <=0 && minutes <= 0)
+            if(seconds > 0) 
+            {
+                seconds -= Time.deltaTime;
+            } 
+            else if(seconds <= 0 && minutes >= 1)
+            {
+                minutes--;
+                seconds = 59;
+            }else if(seconds <= 0 && minutes <= 0)
             {
                 timerHasStarted = false;
+                if(sessionAmount > 0)
+                {
+                    breakTimerHasStarted = true;
+                }
             }
+        } 
+        else if(breakTimerHasStarted && sessionAmount > 0)
+        {
+            panel.color = breakBlue;
+            minutes = rememberMinutes;
+            seconds = rememberSeconds;
+
+            if (breakSeconds > 0)
+            {
+                breakSeconds -= Time.deltaTime;
+            }
+            else if (breakSeconds <= 0 && breakMinutes >= 1)
+            {
+                breakMinutes--;
+                breakSeconds = 59;
+            }
+            else if (breakSeconds <= 0 && breakMinutes <= 0 && sessionAmount > 0)
+            {
+                sessionAmount--;
+                timerHasStarted = true;
+                breakTimerHasStarted = false;
+            }
+            else if(sessionAmount <=0)
+            {
+                minutes = rememberMinutes;
+                seconds = rememberSeconds;
+                timerHasStarted = false;
+                breakTimerHasStarted = false;
+            }
+        }
+        else if(!timerHasStarted && !breakTimerHasStarted && sessionAmount <= 0)
+        {
+            panel.color = mainGreen;
         }
     }
 }
--- ProjectStartUp/Assets/Scripts/TimerButtons.cs
+++ ProjectStartUp/Assets/Scripts/TimerButtons.cs
@@ -6,25 +6,189 @@ public class TimerButtons : MonoBehaviour
 {
     [SerializeField] Timer timer;
 
-    public void Add5Minutes()
+    public void Add10Minutes(bool isForMain)
     {
-        if(timer.timerHasStarted == false)
+        if (isForMain)
         {
-            timer.minutes += 5;
+            if (timer.timerHasStarted == false && timer.breakTimerHasStarted == false)
+            {
+                timer.minutes += 10;
+            }
+        }
+        else if (!isForMain)
+        {
+            if (timer.timerHasStarted == false && timer.breakTimerHasStarted == false)
+            {
+                timer.breakMinutes += 10;
+            }
         }
     }
 
-    public void Add1Minute()
+    public void Add1Minute(bool isForMain)
     {
-        if (timer.timerHasStarted == false)
+        if (isForMain)
         {
-            timer.minutes += 1;
+            if (timer.timerHasStarted == false && timer.breakTimerHasStarted == false)
+            {
+                timer.minutes += 1;
+            }
+        }
+        else if (!isForMain)
+        {
+            if (timer.timerHasStarted == false && timer.breakTimerHasStarted == false)
+            {
+                timer.breakMinutes += 1;
+            }
+        }
+    }
+
+    public void Add10Seconds(bool isForMain)
+    {
+        if (isForMain)
+        {
+            if (timer.seconds < 60 && timer.timerHasStarted == false && timer.breakTimerHasStarted == false)
+            {
+                timer.seconds += 10;
+            }
+            else if (timer.seconds >= 60 && timer.timerHasStarted == false && timer.breakTimerHasStarted == false)
+            {
+                timer.minutes += 1;
+                timer.seconds = 0;
+            }
+        }
+        else if (!isForMain)
+        {
+            if (timer.breakSeconds < 60 && timer.timerHasStarted == false && timer.breakTimerHasStarted == false)
+            {
+                timer.breakSeconds += 10;
+            }
+            else if (timer.seconds >= 60 && timer.timerHasStarted == false && timer.breakTimerHasStarted == false)
+            {
+                timer.breakMinutes += 1;
+                timer.breakSeconds = 0;
+            }
+        }
+    }
+
+    public void Add1Second(bool isForMain)
+    {
+        if (isForMain)
+        {
+            if (timer.timerHasStarted == false && timer.breakTimerHasStarted == false)
+            {
+                timer.seconds += 1;
+            }
+            else if (timer.seconds >= 60 && timer.timerHasStarted == false && timer.breakTimerHasStarted == false)
+            {
+                timer.minutes += 1;
+                timer.seconds = 0;
+            }
+        }
+        else if (!isForMain)
+        {
+            if (timer.timerHasStarted == false && timer.breakTimerHasStarted == false)
+            {
+                timer.breakSeconds += 1;
+            }
+            else if (timer.seconds >= 60 && timer.timerHasStarted == false && timer.breakTimerHasStarted == false)
+            {
+                timer.breakMinutes += 1;
+                timer.breakSeconds = 0;
+            }
+        }
+    }
+
+    public void Dec10Minutes(bool isForMain)
+    {
+        if (isForMain)
+        {
+            if (timer.minutes >= 10 && timer.timerHasStarted == false && timer.breakTimerHasStarted == false)
+            {
+                timer.minutes -= 10;
+            }
+        }
+        else if (!isForMain)
+        {
+            if (timer.breakMinutes >= 10 && timer.timerHasStarted == false && timer.breakTimerHasStarted == false)
+            {
+                timer.breakMinutes -= 10;
+            }
+        }
+    }
+    public void Dec1Minute(bool isForMain)
+    {
+        if (isForMain)
+        {
+            if (timer.minutes >= 1 && timer.timerHasStarted == false && timer.breakTimerHasStarted == false)
+            {
+                timer.minutes -= 1;
+            }
+        }
+        else if (!isForMain)
+        {
+            if (timer.breakMinutes >= 1 && timer.timerHasStarted == false && timer.breakTimerHasStarted == false)
+            {
+                timer.breakMinutes -= 1;
+            }
+        }
+    }
+    public void Dec10Seconds(bool isForMain)
+    {
+        if (isForMain)
+        {
+            if (timer.seconds >= 10 && timer.timerHasStarted == false && timer.breakTimerHasStarted == false)
+            {
+                timer.seconds -= 10;
+            }
+        }
+        else if (!isForMain)
+        {
+            if (timer.breakSeconds >= 10 && timer.timerHasStarted == false && timer.breakTimerHasStarted == false)
+            {
+                timer.breakSeconds -= 10;
+            }
+        }
+    }
+    public void Dec1second(bool isForMain)
+    {
+        if (isForMain)
+        {
+            if (timer.seconds >= 1 && timer.timerHasStarted == false && timer.breakTimerHasStarted == false)
+            {
+                timer.seconds -= 1;
+            }
+        }
+        else if(!isForMain)
+        {
+            if (timer.breakSeconds >= 1 && timer.timerHasStarted == false && timer.breakTimerHasStarted == false)
+            {
+                timer.breakSeconds -= 1;
+            }
+        }
+    }
+
+    public void ChangeSessionAmmount(bool isIncreasing)
+    {
+        if (isIncreasing)
+        {
+            timer.sessionAmount++;
+        }
+        else if(!isIncreasing && timer.sessionAmount > 0)
+        {
+            timer.sessionAmount--;
         }
     }
 
     public void StartTimer()
     {
+        timer.sessionAmount--;
+
         timer.timerHasStarted = true;
+        timer.rememberMinutes = timer.minutes;
+        timer.rememberSeconds = timer.seconds;
+
+        timer.rememberBreakMinutes = timer.breakMinutes;
+        timer.rememberBreakSeconds = timer.breakSeconds;
     }
 
     public void ResetTimer()
