b7c93e552f8472ef2057ea33dd93f0c7f9e94d79
272Jayden <jaydenfokkink@gmail.com>
2023-11-30 14:21:13 +0100

    some stuff

--- ProjectStartUp/Assets/Scripts/Flower.cs
+++ ProjectStartUp/Assets/Scripts/Flower.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,7 +10,9 @@ public class Flower : MonoBehaviour
     //to store information about the flower that needs to change behind the scene/when the user is not inside the garden
     [SerializeField] Button button;
     [SerializeField] GardenManager gardenManager;
-    [SerializeField] Image needsIcon;
+    [SerializeField] GameObject needsIcon;
+    [SerializeField] private TMP_Text growthTimerUI;
+    [SerializeField] GameObject growthTimerUIBlock;
 
     public bool hasCried = false;
     public bool isSunned = false;
@@ -26,26 +29,34 @@ public class Flower : MonoBehaviour
     public Sprite needWater;
     public Sprite needSun;
 
-    public float growthTimer;
+    public float growthTimer = 10;
+    public float roundedGrowthTimer;
 
     // Start is called before the first frame update
     void Start()
     {
+        growthTimer = 10;
+        growthTimerUIBlock.SetActive(false);
         currentState = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (hasCried && isSunned && currentState >= 3 && growthTimer > 10)
+        roundedGrowthTimer = Mathf.RoundToInt(growthTimer);
+
+        if (isSeeded && hasCried && isSunned && currentState >= 3 && growthTimer <= 0)
         {
             currentState = 4;
             button.GetComponent<Image>().sprite = state4;
+            growthTimerUIBlock.SetActive(false);
             rewardIsDropped = true;
         }
-        else if (hasCried && isSunned && currentState >= 2)
+        else if (isSeeded && hasCried && isSunned && currentState >= 2)
         {
-            growthTimer += Time.deltaTime;
+            growthTimerUIBlock.SetActive(true);
+            growthTimer -= Time.deltaTime;
+            growthTimerUI.text = roundedGrowthTimer.ToString();
             currentState = 3;
             button.GetComponent<Image>().sprite = state3;
         }
@@ -57,15 +68,19 @@ public class Flower : MonoBehaviour
 
         if (!isSeeded)
         {
+            needsIcon.SetActive(true);
             needsIcon.GetComponent<Image>().sprite = needSeeds;
         }
-        else if (!hasCried)
+        else if (!hasCried && isSeeded)
         {
             needsIcon.GetComponent<Image>().sprite = needWater;
         }
-        else if (!isSunned)
+        else if (!isSunned && hasCried && isSeeded)
         {
             needsIcon.GetComponent<Image>().sprite = needSun;
+        } else
+        {
+            needsIcon.SetActive(false);
         }
     }
 
@@ -80,7 +95,7 @@ public class Flower : MonoBehaviour
 
     public void GiveWater()
     {
-        if (gardenManager.isHoldingWater)
+        if (gardenManager.isHoldingWater && isSeeded)
         {
             hasCried = true;
             //gardenManager.hasWater = false;
@@ -89,7 +104,7 @@ public class Flower : MonoBehaviour
 
     public void GiveSun()
     {
-        if (gardenManager.isHoldingSun)
+        if (gardenManager.isHoldingSun && hasCried)
         {
             isSunned = true;
             //gardenManager.hasSun = false;
@@ -100,10 +115,11 @@ public class Flower : MonoBehaviour
     {
         if(rewardIsDropped)
         {
+            rewardIsDropped = false;
             isSeeded = false;
             hasCried = false;
             isSunned = false;
-            growthTimer = 0;
+            growthTimer = 10;
             currentState = 1;
             button.GetComponent<Image>().sprite = state1;
         }
